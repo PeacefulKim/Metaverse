eb1f8964d65d00547614edb9c0d67329ae530312
PeacefulKim <127909764+PeacefulKim@users.noreply.github.com>
2025-10-24 12:09:28 +0900

    feat FlappyManager

--- Assets/Scripts/Flappy/FlappyManager.cs
+++ Assets/Scripts/Flappy/FlappyManager.cs
@@ -1,8 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class FlappyManager : MonoBehaviour
 {
     static FlappyManager flappyManager;
+    public static FlappyManager instance { get { return flappyManager; } }
+    private int currentScore = 0;
+    private void Awake()
+    {
+        flappyManager = this;
+    }
+    public void GameOver()
+    {
+        Debug.Log("Game Over");
+    }
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+    public void QuitGame()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+    public void AddScore(int score)
+    {
+        currentScore += score;
+        Debug.Log("Score: " + currentScore);
+    }
 }
