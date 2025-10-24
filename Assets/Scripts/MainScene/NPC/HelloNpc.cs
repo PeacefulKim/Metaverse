0635a92595998214271b0031134392278266500f
PeacefulKim <127909764+PeacefulKim@users.noreply.github.com>
2025-10-24 14:47:32 +0900

    feat: NPC에 접근하면 UI

--- Assets/Scripts/MainScene/NPC/HelloNpc.cs
+++ Assets/Scripts/MainScene/NPC/HelloNpc.cs
@@ -4,11 +4,10 @@ using UnityEngine;
 
 public class HelloNpc : MonoBehaviour, Iinteract
 {
-    private string npcName = "작은 태양";
     private string dialogue = "안녕하세요! 편하게 쉬다 가세요!";
 
     public void Interact()
     {
-        Debug.Log($"{npcName}: {dialogue}");
+        Debug.Log(dialogue);
     }
 }
--- Assets/Scripts/MainScene/NPC/NPCBase.cs
+++ Assets/Scripts/MainScene/NPC/NPCBase.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class NPCBase : MonoBehaviour
 {
     private Iinteract interactable;
+    public GameObject speechBubble;
     private bool playerNearby = false;
+    private bool isVisible = false;
     private void Awake()
     {
         interactable = GetComponent<Iinteract>();
@@ -14,11 +16,21 @@ public class NPCBase : MonoBehaviour
             Debug.LogError("Iinteract is null");
         }
     }
+    private void Start()
+    {
+        if (speechBubble != null) speechBubble.SetActive(false);
+    }
     private void Update()
     {
-        if(playerNearby && Input.GetKeyDown(KeyCode.E))
+        if (playerNearby && !isVisible) 
         {
-            interactable.Interact();
+            ShowBubble();
+            if(Input.GetKeyDown(KeyCode.E)) interactable.Interact();
+            return;
+        }
+        else if (!playerNearby && isVisible)
+        {
+            HideBubble();
         }
     }
     private void OnTriggerEnter2D(Collider2D collision)
@@ -28,6 +40,22 @@ public class NPCBase : MonoBehaviour
     private void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.CompareTag("Player")) playerNearby = false;
+    }
+    void ShowBubble()
+    {
+        if (speechBubble != null)
+        {
+            speechBubble.SetActive(true);
+            isVisible = true;
+        }
+    }
+    void HideBubble()
+    {
+        if (speechBubble != null)
+        {
+            speechBubble.SetActive(false);
+            isVisible = false;
+        }
     }
 }
 public interface Iinteract
