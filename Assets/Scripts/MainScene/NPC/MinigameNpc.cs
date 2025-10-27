541cdac3314ddc43ca8525e7f8ad57cb24ccee2f
PeacefulKim <corbata@naver.com>
2025-10-27 19:06:42 +0900

    feat: 메인씬에 최고기록 표시

--- Assets/Scripts/MainScene/NPC/MinigameNpc.cs
+++ Assets/Scripts/MainScene/NPC/MinigameNpc.cs
@@ -1,16 +1,25 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MiniGameNpc : MonoBehaviour, Iinteract
 {
-    public string npcName = "미니게임";
+    public TextMeshProUGUI scoreTxt;
+    private const string BestScoreKey = "BestScore";
+
     public void Interact()
     {
         if (Input.GetKeyDown(KeyCode.E))
         {
             SceneManager.LoadScene("Flappy");
         }
+    }
+    void Start()
+    {
+        int bestScore = PlayerPrefs.GetInt(BestScoreKey, 0);
+        scoreTxt.text = $"Best Score: {bestScore}";
     }
 }
